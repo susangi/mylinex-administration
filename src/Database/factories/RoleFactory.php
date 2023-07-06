560d34c2f3da5cb7f1f26a8b5f8900fd99092942
Susangi Charuni <susangi@mylinex.com>
2023-07-06 15:38:23 +0530

    build : Role factory build

---
+++ src/Database/factories/RoleFactory.php
@@ -0,0 +1,23 @@
+<?php
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Administration\Models\Role;
+use Administration\Models\Permission;
+
+class RoleFactory extends Factory
+{
+    protected $model = Role::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'guard_name' => 'web'
+        ];
+    }
+}
